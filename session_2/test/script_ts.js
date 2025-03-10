45e31e397aecad84291f8b3870f9b399950ba980
Andrei Navumau <tyziaa@gmail.com>
2025-03-10 07:52:55 -0400

    Add object destrucuring to session 1

--- session_2/test/script_ts.js
+++
@@ -1,6 +0,0 @@
-function changeText() {
-    var message = "Hello, you clicked the button!";
-    var paragraph = document.getElementById("demo");
-    paragraph.innerHTML = message;
-    console.log("Text has been changed.");
-}
